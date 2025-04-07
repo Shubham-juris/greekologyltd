4475587d0fc29d1c5cf53b8de4dc5580502d9821
It-juris <itjurisnexa@gmail.com>
2025-04-07 10:06:01 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 class='text-3xl font-bold underline text-center mt-14 py-14 bg-stone-600'>
