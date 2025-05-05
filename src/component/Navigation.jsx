26f20c5b0a1116726aafe2ddd707199f519fb374
ashishjuris <ashish.juris@gmail.com>
2025-05-05 12:42:39 +0530

    final changes in logo

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import logo from "../../src/assets/Logo/greeklogy.webp"
+import logo from "../../src/assets/Logo/Logo.png";
 
 const Navigation = () => {
   return (
